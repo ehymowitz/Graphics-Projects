ced53e29ccacc0214a22f39313e5f3f76437522a
ehymowitz <ehymowitz@gmail.com>
2020-01-30 16:03:56 -0500

    Ballrings done

--- p5/BallRings/BallRings.js
+++ p5/BallRings/BallRings.js
@@ -1,9 +1,8 @@
-var canvasX = 640, canvasY = 500
+var canvasX = 500, canvasY = 500
 var ad = 90, amp = 50, ar = 8, ramp = 5, balls = 40, x = canvasX/2, y = canvasY/2;
 
 function setup() {
-  var canvas = createCanvas(canvasX, canvasY);
-  canvas.parent('Ball-Rings');
+  createCanvas(canvasX, canvasY);
   noStroke();
   noCursor();
 }
@@ -13,12 +12,23 @@ function draw(){
   for (var i = 0; i < 10; i++){
     ballring(ad, amp, ar, ramp, map(i, 0, 9, 0.001, .0025), balls, x, y);
   }
+
+if (!mouseIsPressed) {
   push();
-  fill("Black");
-  textSize(12);
-  textAlign(CENTER);
-  text("← Ring Color →, ↑ Ring Size ↓,", mouseX, mouseY);
+  fill(0,0,0)
+  circle(mouseX, mouseY, 5);
   pop();
+}
+
+  if (mouseIsPressed) {
+    push();
+    fill("Black");
+    textSize(12);
+    textAlign(CENTER);
+    text("← Ring Color →, ↑ Ring Size ↓,", mouseX, mouseY);
+    pop();
+ }
+
 }
 
 function ballring(ad, amp, ar, ramp, freq, balls, x, y){
@@ -33,7 +43,7 @@ function ballring(ad, amp, ar, ramp, freq, balls, x, y){
   var radius = ar - ramp*sin(freq*millis());
   var color = 255-255/2*sin(freq*millis());
   for (var j=0; j<balls; j++){
-    fill(color, mouseFactorX, 50);
+    fill(color, mouseFactorX, mouseFactorX);
     circle(x+distance*cos(j+map(j,0,balls,0,2*PI)), y+distance*sin(j+map(j,0,balls,0,2*PI)), radius);
   }
 }
--- p5/ParticlePlay/ParticlePlay.js
+++ p5/ParticlePlay/ParticlePlay.js
@@ -1,14 +1,19 @@
 var canvasX = 640, canvasY = 400;
 
 // Particle Tracking
-var particleR = [], particleX = [], particleY = [];
+var particleX = [], particleY = [], velocityX = [], velocityY =[], particleR = [];
+// Mouse Componenet
+var vXm = [], vYm = [], aXm = [], aYm = [];
+// Other Particles Component
+var vXp = [], vYp = [];
+
+// Numbers Tracking
 var particlesMade = 0;
 var maxParticles = 100;
 var lastTime = 0, threshold = 0;
 
 function setup() {
-  var canvas = createCanvas(canvasX, canvasY);
-  canvas.parent('Particle-Play');
+  createCanvas(canvasX, canvasY);
   noStroke();
   noCursor();
 }
@@ -18,25 +23,56 @@ function draw() {
 
   fill(100,40,200,255);
   circle(mouseX, mouseY, 10)
+  noStroke();
 
   push();
   fill(64, 255, 255, 130);
-  for (var i = 0; i <= particlesMade; i++){
-    circle(particleX[i], particleY[i], particleR[i]);
-  }
-  pop();
+  // Mouse Componenet
+  for (var i = 0; i < particlesMade; i++){
 
+    if (particlesMade > 0){
+      var dXm = particleX[i] - mouseX;
+      var dYm = particleY[i] - mouseY;
+
+      var distance = sqrt(dXm*dXm + dYm*dYm);
+      if (distance < 1) distance = 1;
+
+      var fm = distance * particleR * 0.005;
+
+      aXm[i] += dXm*fm * 0.005;
+      aYm[i] += dYm*fm * 0.005;
+
+      vXm[i] += aXm[i] * 0.005;
+      vYm[i] += aYm[i] * 0.005;
+
+      console.log(vXm);
+
+    }
+
+  }
+
+  for (var i = 0; i < particlesMade; i++){
+      particleX[i] += vXm[i];
+      particleY[i] += vYm[i];
+
+      circle(particleX[i], particleY[i], particleR[i]);
+    }
+  pop();
 }
 
 function mousePressed(){
   addParticles();
 }
-function mouseDragged(){
-  addParticles();
-}
+
 function addParticles(){
-  particleR.push(random(10,20));
   particleX.push(mouseX);
   particleY.push(mouseY);
+  particleR.push(random(10,20));
+  velocityY.push(0);
+  velocityX.push(0);
+  aXm.push(0);
+  aXm.push(0);
+  vXm.push(0);
+  vYm.push(0);
   particlesMade++;
 }
